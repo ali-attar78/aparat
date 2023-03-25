07741528679ddd78c73a72741651ca76cab6f1c9
Ali Attar <m.aliattarr@gmail.com>
2023-03-25 22:52:51 +0330

    few migrations

--- Aparat/database/migrations/2014_10_12_000000_create_users_table.php
+++ Aparat/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('mobile',13)->unique();
+            $table->string('email',100)->unique();
+            $table->string('name',100);
+            $table->string('password',100);
+            $table->string('avatar',100);
+            $table->string('website');
+            $table->string('verified_code',6)->nullable();
+            $table->timestamp('verify_at')->nullable();
             $table->timestamps();
+
         });
     }
 
