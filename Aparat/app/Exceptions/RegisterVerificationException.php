4ce17597748cad7b67e2181890431a346fb97830
Ali Attar <m.aliattarr@gmail.com>
2023-03-27 20:08:16 +0330

    register exceptions

---
+++ Aparat/app/Exceptions/RegisterVerificationException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class RegisterVerificationException extends Exception
+{
+    /**
+     * Report the exception.
+     */
+    public function report(): void
+    {
+        //
+    }
+
+    /**
+     * Render the exception as an HTTP response.
+     */
+    public function render(Request $request): Response
+    {
+        return response([
+            'message'=>$this->getMessage()
+        ],400);
+        dd($request);
+    }
+}
